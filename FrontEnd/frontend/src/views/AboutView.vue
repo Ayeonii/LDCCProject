7af438bbeb76530a7ad2c95220749e9e2e9e04c8
ruddls00114 <kkmm114@gmail.com>
2019-07-30 14:28:53 +0900

    kyoungin

--- FrontEnd/frontend/src/views/AboutView.vue
+++ FrontEnd/frontend/src/views/AboutView.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+     <coverflow :coverList="coverList" :coverWidth="260" :index="2"></coverflow>
   </div>
+
+  
 </template>
+
+<script>
+import { coverflow } from 'vue-coverflow';
+export default {
+  
+  data(){
+    return{
+
+    coverList: [
+              {
+                cover: 'http://ogu51f989.bkt.clouddn.com/react.png',
+                title: 'React'
+              }, {
+                cover: 'http://ogu51f989.bkt.clouddn.com/angular.png',
+                title: 'Angular'
+              }, {
+                cover: 'http://ogu51f989.bkt.clouddn.com/vue.png',
+                title: 'Vue'
+              }, {
+                cover: 'http://ogu51f989.bkt.clouddn.com/webpack.png',
+                title: 'Webpack'
+              }, {
+                cover: 'http://ogu51f989.bkt.clouddn.com/yarn.png',
+                title: 'Yarn'
+              }, {
+                cover: 'http://ogu51f989.bkt.clouddn.com/node.png',
+                title: 'Node'
+              }
+            ]
+          }
+        },
+        components: {
+          coverflow
+        },
+        methods :{
+
+        }
+
+}
+</script>
