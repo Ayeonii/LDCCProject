9713ea94ec94bf95e377cc279c9b5f2a47542b98
ruddls00114 <kkmm114@gmail.com>
2019-07-31 10:53:10 +0900

    setup nodemon & merge

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -24,6 +24,7 @@ app.use('/', indexRouter);
 app.use('/api/foodList', foodRouter);
 app.use('/api/payment', paymentRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
